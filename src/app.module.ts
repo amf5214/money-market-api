39bff3ab665bcfac03ce6b4f43ede72498a8749f
amf5214 <austinfraley.lee@gmail.com>
2024-02-16 11:04:40 -0500

    Imported and configured new modules
    Imported and configured new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { NestModule } from './nest/nest.module';
+import { NewsModule } from './news/news.module';
+import { StockdataModule } from './stockdata/stockdata.module';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
+  imports: [AuthModule, UserModule, NestModule, NewsModule, StockdataModule, ProfileModule],
   controllers: [],
   providers: [],
 })
