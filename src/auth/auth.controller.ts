869416b992d4d39d00c4be23696f819b0ae8872c
amf5214 <austinfraley.lee@gmail.com>
2024-02-17 21:43:29 -0500

    Fixed bug with auth controller
    Fixed bug with auth controller by adding necessary imports for Post and Body

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 
@@ -7,12 +7,10 @@ export class AuthController {
 	constructor(private authService:AuthService) {}
 
 	@Post('signup')
-	signup(@Body dto:AuthDto) {
 		return this.authService.signup(dto);
 	}
 
 	@Post('signin')
-	signin(@Body dto:AuthDto) {
 		return this.authService.signin(dto);
 	}
 }
