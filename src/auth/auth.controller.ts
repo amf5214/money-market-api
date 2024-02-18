ac3bcd75b1c5bc850b7e3689b07f5153360c1efd
amf5214 <austinfraley.lee@gmail.com>
2024-02-17 21:43:49 -0500

    Fixed bug with auth controller
    Fixed bug with auth controller by adding parenthesis to the Body decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,10 +7,12 @@ export class AuthController {
 	constructor(private authService:AuthService) {}
 
 	@Post('signup')
+	signup(@Body() dto:AuthDto) {
 		return this.authService.signup(dto);
 	}
 
 	@Post('signin')
+	signin(@Body() dto:AuthDto) {
 		return this.authService.signin(dto);
 	}
 }
