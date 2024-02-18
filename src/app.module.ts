71520edb6d2c836f13ea1ef070cb54f297b67657
amf5214 <austinfraley.lee@gmail.com>
2024-02-17 21:30:19 -0500

    Added global config module
    Added global config module so that PrismaService can access config and PrismaService is accessible globally

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { NestModule } from './nest/nest.module';
 import { NewsModule } from './news/news.module';
 import { StockdataModule } from './stockdata/stockdata.module';
 import { ProfileModule } from './profile/profile.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule, NestModule, NewsModule, StockdataModule, ProfileModule, PrismaModule],
+  imports: [
+    AuthModule, 
+    UserModule, 
+    NewsModule, 
+    StockdataModule, 
+    ProfileModule, 
+    PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
