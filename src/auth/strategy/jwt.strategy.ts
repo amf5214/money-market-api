0d33a599dd0c3a2029c0c9c3d8867167e3778ead
amf5214 <austinfraley.lee@gmail.com>
2024-02-19 15:49:31 -0500

    Added documentation to the JwtStrategy class
    Added documentation to the JwtStrategy class

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -5,27 +5,37 @@ import { ConfigService } from '@nestjs/config';
 import { PrismaService } from '../../prisma/prisma.service';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
+// Class to handle jwt objects
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
+	// Constructor function that specifies dependencies for DI
 	constructor(
 		private jwt:JwtService,
 		private config:ConfigService,
 		private prisma:PrismaService,
 	) {
 
+		// Super function to pass arguements to super class
 		super({
+			// Parameter to specify how to get the jwt from the request
 			jwtFromRequest:
 				ExtractJwt.fromAuthHeaderAsBearerToken(),
+			// Parameter to specify the jwt for token creation
 			secretOrKey: config.get('JWT_SECRET'),
 		});
 	}
 
+	// Validation function so that the strategy can check if the token is legit
 	async validate(payload: {sub:number, email:string}) {
+		// Function to grab the authAccount object from the database using the email in the token
 		const auth = await this.prisma.authAccount.findUnique({
 			where: {
 				id:payload.sub,
 			}
 		})
+
+		// Deleting hash attribute from the object so that the password hash is not sent with 
+		// the response
 		delete auth.hash;
 
 		return auth;
