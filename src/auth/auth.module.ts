cea9a90148350ceb57983161116c377370aeef5f
amf5214 <austinfraley.lee@gmail.com>
2024-02-19 13:18:01 -0500

    Added the import for the jwt module
    Added the import for the jwt module and included the configuration of the JwtStrategy as a provider

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '../prisma/prisma.module';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategy';
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+  imports: [JwtModule.register({})],
+  providers: [AuthService, JwtStrategy],
+  controllers: [AuthController],
 })
-export class AuthModule {}
+export class AuthModule {
+  
+}
