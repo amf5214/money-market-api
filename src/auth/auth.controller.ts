e9f5ce51f04b8d31e89c007fa33db04f67686733
amf5214 <austinfraley.lee@gmail.com>
2024-02-19 15:51:54 -0500

    Added documentation to the auth controller
    Added documentation to the auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,15 +2,22 @@ import { Controller, Post, Body, HttpStatus, HttpCode } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 
+// Class to handle routing of auth path
 @Controller('auth')
 export class AuthController {
+	// Constructor function that specifies dependencies for DI
 	constructor(private authService:AuthService) {}
 
+	// signup route within the auth path
+	// Specifies success http codde with @HttpCode decorator
 	@HttpCode(HttpStatus.CREATED)
 	@Post('signup')
 	signup(@Body() dto:AuthDto) {
 		return this.authService.signup(dto);
 	}
+
+	// signin route within the auth path
+	// Specifies success http codde with @HttpCode decorator
 	@HttpCode(HttpStatus.OK)
 	@Post('signin')
 	signin(@Body() dto:AuthDto) {
