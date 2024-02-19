6c30246d633cb09c6415499607ed23258e0848f9
amf5214 <austinfraley.lee@gmail.com>
2024-02-19 13:14:55 -0500

    Created and export the JwtStrategy class
    Created and export the JwtStrategy class as well as added the base constructor to allow for the dependency injection

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -4,3 +4,13 @@ import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import { PrismaService } from '../../prisma/prisma.service';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+	constructor(
+		private jwt:JwtService,
+		private config:ConfigService,
+		private prisma:PrismaService,
+	) {
+	}
+}
