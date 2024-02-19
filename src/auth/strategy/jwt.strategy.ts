f42f2769669f9275325c6176656fe8688ba57ec0
amf5214 <austinfraley.lee@gmail.com>
2024-02-19 13:13:10 -0500

    Added necessary imports for the jwt.strategy file
    Added necessary imports for the jwt.strategy file

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,1 +1,6 @@
 import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { PrismaService } from '../../prisma/prisma.service';
+import { ExtractJwt, Strategy } from 'passport-jwt';
