8a10531ced9e77e4245468f0f284ced7b4039c79
amf5214 <austinfraley.lee@gmail.com>
2024-02-20 14:27:15 -0500

    Created base learn service methods

--- src/learn/learn.service.ts
+++ src/learn/learn.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class LearnService {}
+export class LearnService {
+	constructor() {}
+
+	home() {
+		return {message:''};
+	}
+
+	loadseries(seriesId:number) {
+		return {message:''};
+	}
+
+	loadpage(pageId:number) {
+		return {message:''};
+	}
+}
