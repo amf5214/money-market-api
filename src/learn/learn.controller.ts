555025f7e7d746ee012f407bf7a3c73422913de4
amf5214 <austinfraley.lee@gmail.com>
2024-02-20 14:11:05 -0500

    feat: add new routes for home, series, and page services in LearnController
    The changes were made to add new routes for the home, series, and page services in the LearnController. These routes are used to handle requests related to the home, series, and page services respectively.

--- src/learn/learn.controller.ts
+++ src/learn/learn.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get, Param, UseGuards } from '@nestjs/common';
 import { JwtGuard } from '../auth/guard';
 import { LearnService } from './learn.service';
 
@@ -7,6 +7,21 @@ import { LearnService } from './learn.service';
 @Controller('learn')
 export class LearnController {
 	constructor(private learnService: LearnService) {}
+
+	@Get('home')
+	home() {
+		return {message: 'This is a test path for home service. Request=/learn/home'};
+	}
+
+	@Get('series/:id')
+	seriesload(@Param() params: any) {
+		return {message: `This is a test path for series service. Request=/learn/series/${params.id}`};
+	}
+
+	@Get('page/:id')
+	pageload(@Param() params: any) {
+		return {message: `This is a test path for page service. Request=/learn/page/${params.id}`};
+	}
 
 
 }
