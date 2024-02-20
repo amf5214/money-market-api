82164dac8851c3dd37f85ccf93b7ba6c73dc71ee
amf5214 <austinfraley.lee@gmail.com>
2024-02-20 13:38:55 -0500

    Added jwt guard to the learning controller for authentication

--- src/learn/learn.controller.ts
+++ src/learn/learn.controller.ts
@@ -2,5 +2,7 @@ import { Controller } from '@nestjs/common';
 import { JwtGuard } from '../auth/guard';
 import { LearnService } from './learn.service';
 
+
+@UseGuards(JwtGuard)
 @Controller('learn')
 export class LearnController {}
