be1f92a89585870154ddbb5f12273fbc0ee308f2
amf5214 <austinfraley.lee@gmail.com>
2024-02-21 00:43:36 -0500

    Created profile service to interface with mongodb profile model

---
+++ src/mongodb/profile/profile.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Profile } from '../entities/profile.entity';
+import { CreateProfileDto } from './create-profile-dto';
+
+@Injectable()
+export class ProfileService {
+  constructor(
+    @InjectRepository(Profile)
+    private profilesRepository: Repository<Profile>,
+  ) {}
+
+  async createOne(dto:CreateProfileDto) {
+    const newProfile:Profile = new Profile();    
+    this.profilesRepository.save(newProfile);
+  }
+
+  async editOne(id:number, dto:CreateProfileDto) {
+    this.profilesRepository.update(id, dto);
+  }
+
+  findAll(): Promise<Profile[]> {
+    return this.profilesRepository.find();
+  }
+
+  findOne(id: number): Promise<Profile | null> {
+    return this.profilesRepository.findOneBy({ id });
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.profilesRepository.delete(id);
+  }
+}
