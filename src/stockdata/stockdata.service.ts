3a10a38bcd9ae8ba2854f93c1849fa14723ff9ef
amf5214 <austinfraley.lee@gmail.com>
2024-02-21 13:19:10 -0500

    Created framework for stockdata service
    Created framework for stockdata service

--- src/stockdata/stockdata.service.ts
+++ src/stockdata/stockdata.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { HttpService } from '@nestjs/axios';
+import { TickerDto } from './ticker-dto';
+import { FinancialsDto } from './financials-dto';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class StockdataService {}
+export class StockdataService {
+}
