d41140ce2af15d70845802bbade20c5861c456d4
amf5214 <austinfraley.lee@gmail.com>
2024-02-21 13:51:21 -0500

    Created framework for news controller
    Created framework for news controller

--- src/news/news.controller.ts
+++ src/news/news.controller.ts
@@ -1,4 +1,10 @@
 import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards, Body } from '@nestjs/common';
+import { JwtGuard } from '../auth/guard';
+import { NewsService } from './news.service';
+import { NewsDto } from './news-dto';
 
 @Controller('news')
-export class NewsController {}
+export class NewsController {
+}
+
