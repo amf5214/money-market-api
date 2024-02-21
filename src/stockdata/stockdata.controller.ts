b007b9fac5015862af481d8a95de1a9fede482ba
amf5214 <austinfraley.lee@gmail.com>
2024-02-21 01:48:38 -0500

    Added jwt authentication to all stockdata paths

--- src/stockdata/stockdata.controller.ts
+++ src/stockdata/stockdata.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { JwtGuard } from '../auth/guard';
 
+@UseGuards(JwtGuard)
 @Controller('stockdata')
 export class StockdataController {}
