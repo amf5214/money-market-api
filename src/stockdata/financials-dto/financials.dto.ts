2a76201962b2f9228fecba45c1dce98527daa610
amf5214 <austinfraley.lee@gmail.com>
2024-02-21 11:08:29 -0500

    Defined financials dto class

--- src/stockdata/financials-dto/financials.dto.ts
+++ src/stockdata/financials-dto/financials.dto.ts
@@ -1,1 +1,12 @@
 import { IsEmail, IsNotEmpty, IsString, IsDate, IsInt } from 'class-validator';
+
+export class FinancialsDto {
+
+	@IsString()
+	@IsNotEmpty()
+	tickerName:string;
+
+	@IsDate()
+	@IsNotEmpty()
+	filingData:Date;
+}
