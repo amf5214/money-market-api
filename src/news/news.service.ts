8dc97a855f6f725c7c5c80eeeef30a6d55530698
amf5214 <austinfraley.lee@gmail.com>
2024-02-21 15:35:41 -0500

    Built out marketnews service
    Built out marketnews service

--- src/news/news.service.ts
+++ src/news/news.service.ts
@@ -20,8 +20,11 @@ export class NewsService {
 
 	}
 
-	marketnews(dto:NewsDto) {
-		return 'Test Path';
+	async marketnews(dto:NewsDto) {
+		const basePath = `https://api.marketaux.com/v1/news/all?countries=${dto.country}&language=${dto.language}&filter_entities=true&limit=${dto.limit}&published_after=${dto.publishedAfterDate}&api_token=${this.config.get('MARKETAUX_API_TOKEN')}`;
+		const observable = this.httpService.get(`${basePath}`);
+		const response = await firstValueFrom(observable); 
+		return response.data;
 	}
 }
 
