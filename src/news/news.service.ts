683c78abfa241bcb9bdd812e95c963e7e6d356ce
amf5214 <austinfraley.lee@gmail.com>
2024-02-21 13:59:52 -0500

    Added missing import for news dto
    Added missing import for news dto

--- src/news/news.service.ts
+++ src/news/news.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
+import { NewsDto } from './news-dto';
 
 @Injectable()
 export class NewsService {
