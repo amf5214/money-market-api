61d90388fb3ab254d51860be3d98365f0c6a7f0a
amf5214 <austinfraley.lee@gmail.com>
2024-02-21 21:23:35 -0500

    Fixed bug in the exposed profile service
    Fixed bug in the exposed profile service where the id should be an ObjectID, not number as mongodb doesn't support auto incremented ids

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -2,8 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { MongodbService } from '../mongodb/mongodb.service';
 import { CreateProfileDto } from '../mongodb/profile/create-profile-dto';
 import { Profile } from '../mongodb/entities/profile.entity';
-
-
+import { ObjectId } from 'typeorm';
 
 @Injectable()
 export class ProfileService {
@@ -15,7 +14,7 @@ export class ProfileService {
     	await this.mongodb.profile().createOne(dto);
     }
 
-	async editone(id:number, dto:CreateProfileDto) {
+	async editone(id:ObjectId, dto:CreateProfileDto) {
 		await this.mongodb.profile().editOne(id, dto);
 	}
 
@@ -23,11 +22,11 @@ export class ProfileService {
 		return this.mongodb.profile().findAll();
 	}
 
-	findone(id: number): Promise<Profile | null> {
+	findone(id: ObjectId): Promise<Profile | null> {
 		return this.mongodb.profile().findOne(id);
 	}
 
-	async remove(id: number){
+	async remove(id: ObjectId){
 		await this.mongodb.profile().remove(id);
 	}
 }
