b0d68005e5ddafa9fae685d7d1255a316ef3479e
amf5214 <austinfraley.lee@gmail.com>
2024-02-21 19:59:29 -0500

    Created custom configservice
    Created custom configservice

---
+++ src/localconfig/localconfig.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+import * as fs from 'fs';
+import * as path from 'path';
+
+@Injectable()
+export class LocalConfigService {
+}
