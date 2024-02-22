28b6698f67f97cede50070c898eeb1727869945f
amf5214 <austinfraley.lee@gmail.com>
2024-02-21 20:57:54 -0500

    Added necessary imports to profile controller
    Added necessary imports to profile controller

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,4 +1,7 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Post, Get, UseGuards, Param, Patch } from '@nestjs/common';
+import { JwtGuard } from '../auth/guard';
+import { CreateProfileDto } from '../mongodb/profile/create-profile-dto';
+import { ProfileService } from './profile.service';
 
 @Controller('profile')
 export class ProfileController {}
