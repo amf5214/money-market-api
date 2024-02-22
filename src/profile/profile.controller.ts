f86c9c478d3354a698e6c1a453c80ca0adb7da9c
amf5214 <austinfraley.lee@gmail.com>
2024-02-21 20:59:00 -0500

    Added paths to profile controller
    Added paths to profile controller mapped to corresponding profile service functions

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -9,4 +9,29 @@ export class ProfileController {
 	constructor(
 		private profileService:ProfileService,
 	) {}
+
+	@Post('create')
+	async createone(@Body() dto:CreateProfileDto) {
+		return this.profileService.createone(dto);
+	}
+
+	@Patch('update/:id')
+	async editone(@Body() dto:CreateProfileDto, @Param() params:any) {
+		return this.profileService.editone(params.id, dto);
+	}
+
+	@Get('showall')
+	findall() {
+		return this.profileService.findall();
+	}
+
+	@Get(':id')
+	findone(@Param() params:any) {
+		return this.profileService.findone(params.id);
+	}
+
+	@Get('remove/:id')
+	remove(@Param() params:any) {
+		return this.profileService.remove(params.id);
+	}
 }
