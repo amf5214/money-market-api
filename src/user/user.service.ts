8a6bf5609a31184cf51969ca3637b162486b4024
amf5214 <austinfraley.lee@gmail.com>
2024-02-22 23:35:51 -0500

    Added necessary service function to the user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
 
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateUserDto } from './create-user-dto';
+import { UpdateUserDto } from './update-user-dto';
+
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(private prisma:PrismaService) {}
+
+    getuser(id:number) {
+        return '';
+    }
+
+    updateuser(dto:UpdateUserDto) {
+        return '';
+    }
+
+    createuser(dto:CreateUserDto) {
+        return '';
+    }
+}
