4a1aef24fe264416142298d162c5c78a6bc9c4dc
amf5214 <austinfraley.lee@gmail.com>
2024-02-22 23:34:08 -0500

    Created dto to handle profile creation request

---
+++ src/profile/dto/profile.dto.ts
@@ -0,0 +1,40 @@
+import { IsEmail, IsString, IsNumber, IsOptional } from 'class-validator';
+
+// Creates a data transfer object (DTO) for sign in and sign up
+// DTO will verify that an email object stored as a string 
+// and a password object as a string are taken in
+export class ProfileDto {
+
+	@IsString()
+	@IsOptional()
+	name:string;
+
+	@IsString()
+	@IsOptional()
+	bio:string;
+
+	@IsNumber()
+	@IsOptional()
+	age:number;
+
+	@IsString()
+	@IsOptional()
+	experience:string;
+
+	@IsString()
+	@IsOptional()
+	citystate:string;
+
+	@IsString()
+	@IsOptional()
+	education:string;
+
+	@IsEmail()
+	@IsOptional()
+	email:string;
+
+	@IsNumber()
+	@IsOptional()
+	authAccountId:number;
+
+}
