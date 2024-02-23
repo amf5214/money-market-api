b0b23c2d0218ba3243ab814f4926c805f307f057
amf5214 <austinfraley.lee@gmail.com>
2024-02-22 23:37:11 -0500

    Added necessary routes to the user controller and utilitzed the user service functions

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards, Get, Patch, Post, Param, Body } from '@nestjs/common';
 
+import { JwtGuard } from '../auth/guard';
+import { UserService } from './user.service';
+import { CreateUserDto } from './create-user-dto';
+import { UpdateUserDto } from './update-user-dto';
+
+@UseGuards(JwtGuard)
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private userService:UserService) {}
+
+    @Get(':id')
+    getuser(@Param() params:any) {
+        return this.userService.getuser(params.id);
+    }
+
+    @Patch('update')
+    updateprofile(@Body() dto:UpdateUserDto) {
+        return this.userService.updateuser(dto);
+    }
+
+    @Post('create')
+    createaccount(@Body() dto:CreateUserDto) {
+        return this.userService.createuser(dto);
+    }
+}
