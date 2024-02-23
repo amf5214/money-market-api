e0108c630d2c9098a471a62c160507afb81939c2
amf5214 <austinfraley.lee@gmail.com>
2024-02-23 12:43:34 -0500

    Renamed auth/decorator to auth/auth-decorator to account for the need for a second decorator folder used to get the user object from the auth account object retrieved from the request

--- src/auth/auth-decorator/get-auth.decorator.ts
+++ src/auth/auth-decorator/get-auth.decorator.ts
@@ -1,6 +1,6 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-export const GetAuthAccountData = createParamDecorator(
+export const GetAuthAccount = createParamDecorator(
 	(data: string | undefined, ctx: ExecutionContext) => {
 		const request:Express.Request = ctx
 			.switchToHttp()
