ad6cd841ff3a4d0c97ca33a59c02101a5623dfdf
amf5214 <austinfraley.lee@gmail.com>
2024-02-22 19:41:34 -0500

    Removed unused imports and merged duplicate package imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from '../prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
+
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategy';
-import { MongodbService } from '../mongodb/mongodb.service';
 
 @Module({
   imports: [JwtModule.register({})],
