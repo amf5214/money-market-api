3962374ee8828846bd7885a104a68c078b6a2bbb
amf5214 <austinfraley.lee@gmail.com>
2024-02-22 19:43:13 -0500

    Removed unused imports and merged duplicate package imports

--- src/mongodb/mongodb.controller.ts
+++ src/mongodb/mongodb.controller.ts
@@ -1,4 +1,5 @@
 import { Controller } from '@nestjs/common';
+
 import { MongodbService } from './mongodb.service';
 
 @Controller('mongodb')
--- src/mongodb/mongodb.module.ts
+++ src/mongodb/mongodb.module.ts
@@ -1,8 +1,9 @@
 import { Module, Global } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { MongodbController } from './mongodb.controller';
 import { MongodbService } from './mongodb.service';
 import { ProfileService } from './profile/profile.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Profile } from './entities/profile.entity'
 
 @Global()
