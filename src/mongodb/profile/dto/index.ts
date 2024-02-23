d133a135327194ee42e925e0903a883a37894d23
amf5214 <austinfraley.lee@gmail.com>
2024-02-23 14:37:49 -0500

    Refactored CreateProfileDto and corresponding files to just be ProfileDto

--- src/mongodb/profile/dto/index.ts
+++ src/mongodb/profile/dto/index.ts
@@ -1,2 +1,2 @@
 // Bare export that sets the primary class within this package as AuthDto
-export * from './create-profile.dto';
+export * from './profile.dto';
--- src/mongodb/profile/dto/profile.dto.ts
+++ src/mongodb/profile/dto/profile.dto.ts
@@ -3,7 +3,7 @@ import { IsEmail, IsString, IsNumber, IsOptional } from 'class-validator';
 // Creates a data transfer object (DTO) for sign in and sign up
 // DTO will verify that an email object stored as a string 
 // and a password object as a string are taken in
-export class CreateProfileDto {
+export class ProfileDto {
 
 	@IsString()
 	@IsOptional()
