4cd7bcd8485b0dff211d56cff2183ce765755c92
amf5214 <austinfraley.lee@gmail.com>
2024-02-23 13:56:18 -0500

    Revert "Renamed auth/decorator to auth/auth-decorator to account for the need for a second decorator folder used to get the user object from the auth account object retrieved from the request"
    This reverts commit e0108c630d2c9098a471a62c160507afb81939c2.

--- src/auth/decorator/get-auth.decorator.ts
+++ src/auth/decorator/get-auth.decorator.ts
@@ -1,6 +1,6 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-export const GetAuthAccount = createParamDecorator(
+export const GetAuthAccountData = createParamDecorator(
 	(data: string | undefined, ctx: ExecutionContext) => {
 		const request:Express.Request = ctx
 			.switchToHttp()
