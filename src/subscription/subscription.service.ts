5308751cfec63d2b2d3c9b92805f8f749b7fb817
amf5214 <austinfraley.lee@gmail.com>
2024-02-25 11:29:59 -0500

    Created necessary functions for subscription service
    Created necessary functions for subscription service

--- src/subscription/subscription.service.ts
+++ src/subscription/subscription.service.ts
@@ -6,3 +6,20 @@ import { SubscriptionDto } from './dto';
 
 @Injectable()
 export class SubscriptionService {}
+export class SubscriptionService {
+    constructor(private prisma:PrismaService) {}
+    async validate(id:number) {
+    }
+    async invalidate(id:number) {
+    }
+    async create(id:number, dto:SubscriptionDto) {
+    }
+    async remove(id:number) {
+    }
+    async findOne(id:number) {
+    }
+    async findUserSubscriptions(id:number) {
+    }
+    async update(id:number, dto:SubscriptionDto) {
+    }
+}
