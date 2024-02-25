320d993d8c5a4f27b9e8456f004eca1a07b11366
amf5214 <austinfraley.lee@gmail.com>
2024-02-24 22:37:29 -0500

    Removed unnecessary id field from updateuserdto
    Removed unnecessary id field from updateuserdto

--- src/user/update-user-dto/updateuser.dto.ts
+++ src/user/update-user-dto/updateuser.dto.ts
@@ -2,10 +2,6 @@ import { IsOptional, IsString, IsNotEmpty, IsInt } from 'class-validator';
 
 export class UpdateUserDto {
 
-    @IsNotEmpty()
-    @IsInt()
-    id:number;
-
     @IsOptional()
     @IsString()
     firstName:string;
