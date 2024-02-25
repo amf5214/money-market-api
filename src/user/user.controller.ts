7169178ba872cd05eafce34051105a3feb86b57e
amf5214 <austinfraley.lee@gmail.com>
2024-02-24 22:18:35 -0500

    Added import for casl policy guard to user controller
    Added import for casl policy guard and check policy decorators to user controller to be used in access control

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,14 @@
 import { Controller, UseGuards, Get, Patch, Post, Param, Body } from '@nestjs/common';
+import { User } from '@prisma/client';
 
 import { JwtGuard } from '../auth/guard';
 import { UserService } from './user.service';
 import { CreateUserDto } from './create-user-dto';
 import { UpdateUserDto } from './update-user-dto';
+import { CheckPolicies } from 'src/casl/decorator';
+import { AppAbility } from 'src/casl/casl-ability.factory';
+import { PoliciesGuard } from 'src/casl/guard';
+import { Action } from 'src/casl/action.enum';
 
 @UseGuards(JwtGuard)
 @Controller('user')
