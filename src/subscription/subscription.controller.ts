5c54a9c84ed9e10acdc7879b6b2f91ba66693fb3
amf5214 <austinfraley.lee@gmail.com>
2024-02-25 14:41:13 -0500

    Added necessary routes to the subscription controller
    Added necessary routes to the subscription controller

--- src/subscription/subscription.controller.ts
+++ src/subscription/subscription.controller.ts
@@ -8,4 +8,20 @@ import { SubscriptionDto } from './dto';
 @Controller('subscription')
 export class SubscriptionController {
     constructor(private subscriptionService:SubscriptionService) {}
+
+    @Post('create')
+    createSubscription(@Body() dto:SubscriptionDto, @GetAuthAccount() authAccount:AuthAccount) {
+    }
+    @Get('remove/:id')
+    remove(@Param() params:any, @GetAuthAccount() authAccount:AuthAccount) {
+    }
+    @Post('update') 
+    update(@GetAuthAccount() authAccount:AuthAccount, @Body() dto:SubscriptionDto) {
+    }
+    @Get(':id')
+    getSubscription(@Param() params:any) {
+    }
+    @Get('')
+    getUserSubscriptions(@GetAuthAccount() authAccount:AuthAccount) {
+    }
 }
