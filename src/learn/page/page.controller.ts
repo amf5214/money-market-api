37c532b73ff27e4559e45e3941648fd8b1e16ca7
amf5214 <austinfraley.lee@gmail.com>
2024-02-26 15:47:51 -0500

    Created empty routes within page controller
    Created empty routes within page controller

--- src/learn/page/page.controller.ts
+++ src/learn/page/page.controller.ts
@@ -3,4 +3,22 @@ import { PageService } from './page.service';
 
 @Controller('learn/page')
 export class PageController {
+    constructor(
+        private pageService:PageService,
+    ) {}
+
+    @Get(':id')
+    getpage(@Param() params:any) {
+        return '';
+    }
+
+    @Patch('update/:id')
+    updatepage(@Param() params:any) {
+        return '';
+    }
+
+    @Post('create')
+    createpage() {
+        return '';
+    }
 }
