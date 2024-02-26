2555d00c03a0bdb0a5ecfde5db4aa35533a392a2
amf5214 <austinfraley.lee@gmail.com>
2024-02-26 16:28:25 -0500

    Defined series controller routing parameters
    Defined series controller routing parameters

--- src/learn/series/series.controller.ts
+++ src/learn/series/series.controller.ts
@@ -1,5 +1,8 @@
-import { Controller, Get, Param, Patch, Post } from '@nestjs/common';
-import { SeriesService } from './series.service';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { AuthAccount } from '@prisma/client';
+import { GetAuthAccount } from 'src/auth/decorator';
+import { SeriesDto } from 'src/mongodb/learningseries/dto';
+import { SeriesService } from 'src/mongodb/learningseries/series.service';
 
 @Controller('learn/series')
 export class SeriesController {
@@ -8,17 +11,17 @@ export class SeriesController {
     ) {}
 
     @Get(':id')
-    getseries(@Param() params:any) {
+    getseries(@Param() params:any, @GetAuthAccount() authAccount:AuthAccount) {
         return '';
     }
 
     @Patch('update/:id')
-    updateseries(@Param() params:any) {
+    updateseries(@Param() params:any, @GetAuthAccount() authAccount:AuthAccount, @Body() dto:SeriesDto) {
         return '';
     }
 
     @Post('create')
-    createseries() {
+    createseries(@GetAuthAccount() authAccount:AuthAccount, @Body() dto:SeriesDto) {
         return '';
     }
 }
