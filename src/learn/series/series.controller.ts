cba788014716a5244260c429ffe7e9097e41f1b6
amf5214 <austinfraley.lee@gmail.com>
2024-02-26 15:47:38 -0500

    Created empty routes within series controller
    Created empty routes within series controller

--- src/learn/series/series.controller.ts
+++ src/learn/series/series.controller.ts
@@ -3,4 +3,22 @@ import { SeriesService } from './series.service';
 
 @Controller('learn/series')
 export class SeriesController {
+    constructor(
+        private seriesService:SeriesService,
+    ) {}
+
+    @Get(':id')
+    getseries(@Param() params:any) {
+        return '';
+    }
+
+    @Patch('update/:id')
+    updateseries(@Param() params:any) {
+        return '';
+    }
+
+    @Post('create')
+    createseries() {
+        return '';
+    }
 }
