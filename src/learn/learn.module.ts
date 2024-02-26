7e5728b300f7ad4b563c3a63be72a5ac4ff60625
amf5214 <austinfraley.lee@gmail.com>
2024-02-26 15:48:26 -0500

    imported and implemented nested learning controllers
    imported and implemented nested learning controllers

--- src/learn/learn.module.ts
+++ src/learn/learn.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
-
 import { LearnController } from './learn.controller';
+import { HomeController } from './home/home.controller';
+import { PageController } from './page/page.controller';
+import { SeriesController } from './series/series.controller';
 
 @Module({
-  controllers: [LearnController],
+  controllers: [LearnController, HomeController, PageController, SeriesController],
   providers: [],
+  imports: []
 })
 export class LearnModule {}
