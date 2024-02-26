0903bc70f9a2efe976f3555b99d6edba421f730b
amf5214 <austinfraley.lee@gmail.com>
2024-02-26 16:27:05 -0500

    Exported new mongodb services
    Exported new mongodb services

--- src/mongodb/mongodb.module.ts
+++ src/mongodb/mongodb.module.ts
@@ -3,12 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { ProfileService } from './profile';
 import { Profile } from './entities/profile.entity'
+import { SeriesService } from './learningseries/series.service';
+import { PageService } from './learningpage/page.service';
+import { SectionService } from './learningsection/section.service';
 
 @Global()
 @Module({
   imports: [TypeOrmModule.forFeature([Profile])],
   controllers: [],
-  providers: [ProfileService],
-  exports: [ProfileService],
+  providers: [ProfileService, SeriesService, PageService, SectionService],
+  exports: [ProfileService, SeriesService, PageService, SectionService],
 })
 export class MongodbModule {}
