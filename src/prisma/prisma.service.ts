f566df20b060c8e45bbd6514d1b8a130c769015d
amf5214 <austinfraley.lee@gmail.com>
2024-02-26 12:29:13 -0500

    Added documentation to the prisma module services
    Added documentation to the prisma module services

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { ConfigService } from '@nestjs/config';
 
+// Custom client interface for prisma client to specify the database url
 @Injectable()
 export class PrismaService extends PrismaClient {
 	constructor(config:ConfigService) {
