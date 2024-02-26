57f17647aa69cc376ddc546e72b39298baabea28
amf5214 <austinfraley.lee@gmail.com>
2024-02-26 15:47:17 -0500

    Created series controller within learn module
    Created series controller within learn module to handle routing for learn/series

---
+++ src/learn/series/series.controller.ts
@@ -0,0 +1,6 @@
+import { Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { SeriesService } from './series.service';
+
+@Controller('learn/series')
+export class SeriesController {
+}
