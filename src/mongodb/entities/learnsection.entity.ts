122c465b758a7058a6218ffec07c0dfc39c055a7
amf5214 <austinfraley.lee@gmail.com>
2024-02-26 22:42:35 -0500

    Implemented the learnsection entity class
    Implemented the learnsection entity class

--- src/mongodb/entities/learnsection.entity.ts
+++ src/mongodb/entities/learnsection.entity.ts
@@ -1,8 +1,26 @@
-import { Entity, ObjectIdColumn } from 'typeorm';
+import { Column, Entity, ObjectId, ObjectIdColumn } from 'typeorm';
 
 // Object type declaration for handling mongodb LearnSection objects
 @Entity()
 export class LearnSection {
   @ObjectIdColumn()
-  id: number;
+  id:ObjectId;
+
+  @Column()
+  sectionName:string;
+
+  @Column()
+  idName:string;
+
+  @Column()
+  className:string;
+
+  @Column()
+  innerHtml:string;
+
+  @Column()
+  styleString:string;
+
+  @Column()
+  orderId:number;
 }
