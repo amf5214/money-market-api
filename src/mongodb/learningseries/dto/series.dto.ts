56e55e284b099aacc7f2f6507d0de25fa07726e4
amf5214 <austinfraley.lee@gmail.com>
2024-02-26 22:43:39 -0500

    Implemented a filecontent dto
    Implemented the learning series dto

--- src/mongodb/learningseries/dto/series.dto.ts
+++ src/mongodb/learningseries/dto/series.dto.ts
@@ -1,3 +1,22 @@
+import { IsNumber, IsString } from "class-validator";
+import { ObjectId } from "typeorm";
+
 export class SeriesDto {
-    
+    @IsString()
+    objectId:ObjectId;
+
+    @IsString()
+    title:string;
+
+    @IsString()
+    descriptioin:string;
+
+    @IsString()
+    coverArtId:ObjectId;
+
+    @IsString()
+    authorName:string;
+
+    @IsNumber()
+    authorUserId:number;
 }
