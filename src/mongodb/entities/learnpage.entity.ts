566f3d3f67aed32a098f558c2c6187e2d5504ddf
amf5214 <austinfraley.lee@gmail.com>
2024-02-26 22:42:20 -0500

    Implemented the learnpage entity class
    Implemented the learnpage entity class

--- src/mongodb/entities/learnpage.entity.ts
+++ src/mongodb/entities/learnpage.entity.ts
@@ -1,8 +1,29 @@
-import { Entity, ObjectIdColumn } from 'typeorm';
+import { Column, Entity, ObjectId, ObjectIdColumn } from 'typeorm';
 
 // Object type declaration for handling mongodb LearnPage objects
 @Entity()
 export class LearnPage {
   @ObjectIdColumn()
-  id: number;
+  id:ObjectId;
+
+  @Column()
+  title:string;
+
+  @Column()
+  description:string;
+
+  @Column()
+  coverArtId:ObjectId;
+
+  @Column()
+  authorName:string;
+
+  @Column()
+  authorUserId:number;
+
+  @Column()
+  learningSeriesId:number;
+
+  @Column()
+  orderId:number;
 }
