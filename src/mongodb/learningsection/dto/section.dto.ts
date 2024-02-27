e23c477c8d8c678d022af080fe98fe4e61b3c8f6
amf5214 <austinfraley.lee@gmail.com>
2024-02-26 22:43:51 -0500

    Implemented the learning section dto
    Implemented the learning section dto

--- src/mongodb/learningsection/dto/section.dto.ts
+++ src/mongodb/learningsection/dto/section.dto.ts
@@ -1,3 +1,25 @@
+import { IsNumber, IsString } from "class-validator";
+import { ObjectId } from "typeorm";
+
 export class SectionDto {
-    
+    @IsString()
+    objectId:ObjectId;
+
+    @IsString()
+    sectionName:string;
+
+    @IsString()
+    idName:string;
+
+    @IsString()
+    className:string;
+
+    @IsString()
+    innerHtml:string;
+
+    @IsString()
+    styleString:string;
+
+    @IsNumber()
+    orderId:number;
 }
