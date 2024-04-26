1b1e189252648dbf93990b295bd1382af70bfbb8
RileyJenkins3621 <122946442+RileyJenkins3621@users.noreply.github.com>
2024-04-26 18:02:21 -0400

    Stripe Integration
    Stripe Integration

--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Redirect, HttpStatus } from '@nestjs/common';
+import { PaymentService } from './payment.service';
 
 @Controller('payment')
-export class PaymentController {}
+export class PaymentController {
+  constructor(private paymentService: PaymentService) {}
+
+  @Post('create-checkout-session')
+  @Redirect()
+  async createCheckoutSession() {
+    const checkoutSessionUrl =
+      await this.paymentService.createCheckoutSession();
+    console.log(checkoutSessionUrl);
+    return { url: checkoutSessionUrl, statusCode: HttpStatus.FOUND };
+  }
+}
