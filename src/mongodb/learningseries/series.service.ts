1e31f4e7ae2bbf315dcb3b7b8705c8bee304f5a6
amf5214 <austinfraley.lee@gmail.com>
2024-04-26 20:49:08 -0400

    Implemented typeorm repository in learning series service to be used as an access tool to the mongodb

--- src/mongodb/learningseries/series.service.ts
+++ src/mongodb/learningseries/series.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { SeriesDto } from './dto';
+import { InjectRepository } from "@nestjs/typeorm";
+import { LearnSection } from "../entities/learnsection.entity";
+import { Repository } from "typeorm";
+import { LearnSeries } from "../entities/learnseries.entity";
 
 @Injectable()
 export class SeriesService {
+
+    constructor(
+      @InjectRepository(LearnSeries)
+      private learningSeriesRepository: Repository<LearnSeries>
+    ) {};
 
     getSeries(sectionId:number, authAccountId:number) {
         throw new Error('Method not implemented.');
