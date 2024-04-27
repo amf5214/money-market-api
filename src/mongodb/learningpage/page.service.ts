a439080ca6034697e194769bb94648cd9edbe0fb
amf5214 <austinfraley.lee@gmail.com>
2024-04-26 20:48:34 -0400

    Implemented typeorm repository in page service to be used as an access tool to the mongodb

--- src/mongodb/learningpage/page.service.ts
+++ src/mongodb/learningpage/page.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { PageDto } from './dto';
+import { InjectRepository } from "@nestjs/typeorm";
+import { LearnSection } from "../entities/learnsection.entity";
+import { Repository } from "typeorm";
+import { LearnPage } from "../entities/learnpage.entity";
 
 @Injectable()
 export class PageService {
+
+    constructor(
+      @InjectRepository(LearnPage)
+      private learningPageRepository: Repository<LearnPage>
+    ) {};
     
     getPage(pageId:number, authAccountId:number) {
         throw new Error('Method not implemented.');
