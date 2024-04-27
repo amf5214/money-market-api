00d06f08934345942066beca2809f757b8a2e566
amf5214 <austinfraley.lee@gmail.com>
2024-04-26 21:01:01 -0400

    Created content management controller route function to create series

--- src/contentmanagement/contentmanagement.controller.ts
+++ src/contentmanagement/contentmanagement.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
+import { JwtGuard } from "../auth/guard";
+import { ContentmanagementService } from "./contentmanagement.service";
+import { SeriesDto } from "../mongodb/learningseries/dto";
+import { GetAuthAccount } from "../auth/decorator";
+import { AuthAccount } from "@prisma/client";
 
+@UseGuards(JwtGuard)
 @Controller('contentmanagement')
-export class ContentmanagementController {}
+export class ContentmanagementController {
+  constructor(
+    private contentmanagementService: ContentmanagementService
+  ) {}
+
+  @Post('/series/create')
+  createseries(@Body() dto:SeriesDto, @GetAuthAccount() authAccount:AuthAccount) {
+    this.contentmanagementService.createseries(dto, authAccount.id);
+  }
+}
