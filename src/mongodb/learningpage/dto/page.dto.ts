9d2eb049c91621b9bf79766e1f0df18c4f4020cb
amf5214 <austinfraley.lee@gmail.com>
2024-04-28 14:46:08 -0400

    Updated learn page dto
    Updated learn page dto

--- src/mongodb/learningpage/dto/page.dto.ts
+++ src/mongodb/learningpage/dto/page.dto.ts
@@ -12,18 +12,15 @@ export class PageDto {
     description:string;
 
     @IsString()
-    coverArtId:ObjectId;
-
-    @IsString()
-    authorName:string;
+    coverArtId:number;
 
     @IsNumber()
-    authorUserId:number;
-
-    @IsNumber()
-    learningSeriesId:number;
+    learningSeriesId:ObjectId;
 
     @IsNumber()
     orderId:number;
+
+    @IsString()
+    videoSource:string;
 
 }
