b6a747c85fb46ad7ad8e242e1c267a63105fa2a3
amf5214 <austinfraley.lee@gmail.com>
2024-04-29 11:06:49 -0400

    Moved to find rather than findby due to mongo compatibility
    Moved to find rather than findby because findby uses query builder which is not compatible with mongodb

--- src/mongodb/learningseries/series.service.ts
+++ src/mongodb/learningseries/series.service.ts
@@ -59,6 +59,10 @@ export class SeriesService {
     // Function for finding an author's Series'
     async findAllByAuthor(id: number): Promise<LearnSeries[] | null> {
         const user: User = await this.userService.getownuser(id);
-        return this.learningSeriesRepository.findBy({ authorUserId: user.id });
+        return this.learningSeriesRepository.find({
+            where: {
+                authorUserId: user.id
+            }
+        });
     }
 }
