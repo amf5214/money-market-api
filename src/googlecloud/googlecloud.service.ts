33a0d3ff47e1e3374b2e76b981618a197b1e21d1
amf5214 <austinfraley.lee@gmail.com>
2024-04-29 15:58:40 -0400

    Created service to print out all gcp buckets
    Created service to print out all gcp buckets

--- src/googlecloud/googlecloud.service.ts
+++ src/googlecloud/googlecloud.service.ts
@@ -7,4 +7,19 @@ import *  as mongodb from 'mongodb';
 @Injectable()
 export class GoogleCloudService {
   constructor(private localConfig:LocalConfigService) {}
+
+  async printValidBuckets() {
+    const storage:Storage = new Storage({
+      projectId: this.localConfig.getGoogleCloudId(),
+    });
+    const [buckets] = await storage.getBuckets();
+    console.log('Buckets:');
+
+    for (const bucket of buckets) {
+      console.log(`- ${bucket.name}`);
+    }
+
+    console.log('Listed all storage buckets.');
+  }
+
 }
