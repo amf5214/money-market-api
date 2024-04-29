c5bbd252c30acba30802559ff635b53c64c7c2da
amf5214 <austinfraley.lee@gmail.com>
2024-04-29 10:59:56 -0400

    Converted page dto fields to optional
    Converted page dto fields to optional that way the dto can be used for searching. Without removing them, the api returns a 400 error because there are missing required fields when you try to use the dto as a search tool

--- src/mongodb/learningpage/dto/page.dto.ts
+++ src/mongodb/learningpage/dto/page.dto.ts
@@ -1,24 +1,29 @@
-import { IsEmail, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
 import { ObjectId } from 'typeorm';
 
 export class PageDto {
 
     objectId:ObjectId;
 
+    @IsOptional()
     @IsString()
     title:string;
 
+    @IsOptional()
     @IsString()
     description:string;
 
+    @IsOptional()
     @IsString()
     coverArtId:number;
 
     learningSeriesId:ObjectId;
 
+    @IsOptional()
     @IsNumber()
     orderId:number;
 
+    @IsOptional()
     @IsString()
     videoSource:string;
 
