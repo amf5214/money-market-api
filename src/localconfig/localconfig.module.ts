57a3c145747bc27f01929ccf493d2ab19cf6480d
amf5214 <austinfraley.lee@gmail.com>
2024-04-29 15:57:54 -0400

    Made local config service global
    Made local config service global

--- src/localconfig/localconfig.module.ts
+++ src/localconfig/localconfig.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from "@nestjs/common";
 
 import { LocalConfigService } from './localconfig.service';
 
+@Global()
 @Module({
   providers: [
     {
