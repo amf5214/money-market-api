b3e7367891fb29c3a7a91e02b2e0c2aabdaea6c4
RileyJenkins3621 <122946442+RileyJenkins3621@users.noreply.github.com>
2024-04-30 14:31:35 -0400

    Payment Progress

--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -1,16 +1,23 @@
 import { Controller, Post, Redirect, HttpStatus } from '@nestjs/common';
 import { PaymentService } from './payment.service';
+import { JwtGuard } from 'src/auth/guard';
+import { UseGuards } from '@nestjs/common/decorators';
+import { AuthAccount } from '@prisma/client';
+import { GetAuthAccount } from 'src/auth/decorator';
 
+@UseGuards(JwtGuard)
 @Controller('payment')
 export class PaymentController {
   constructor(private paymentService: PaymentService) {}
 
   @Post('create-checkout-session')
   @Redirect()
-  async createCheckoutSession() {
-    const checkoutSessionUrl =
-      await this.paymentService.createCheckoutSession();
-    console.log(checkoutSessionUrl);
+  async createCheckoutSession(@GetAuthAccount() authAccount: AuthAccount) {
+    const checkoutSessionUrl = await this.paymentService.createCheckoutSession(
+      authAccount.id,
+      authAccount.email,
+    );
+ 
     return { url: checkoutSessionUrl, statusCode: HttpStatus.FOUND };
   }
 }
